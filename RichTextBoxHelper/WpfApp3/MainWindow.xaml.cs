7a7b4dd1e96c26027a626d448dd7678db540ca4a
Tom JD1 Xue <xuejd1@lenovo.com>
2023-07-15 19:41:12 +0800

    add RichTextBoxHelper demo: show binding in multiple RichTextBox

---
+++ RichTextBoxHelper/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    public class ViewModel
+    {
+        public Model model { get; set; }
+
+        public ViewModel()
+        {
+            model = new Model()
+            {
+                ID = "001",
+                Name = "Daisy",
+                Description = "<FlowDocument xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\"><Paragraph Foreground=\"Red\"><Bold>Hello, this is RichTextBox</Bold></Paragraph></FlowDocument>"
+            };
+        }
+    }
+
+    public class Model
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
